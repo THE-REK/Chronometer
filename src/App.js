90e3d547b6a6cbbfcf978387de0afedf78f227a8
THE-REK <eren83086@gmail.com>
2020-11-05 14:51:21 +0300

    initial state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, {useState} from "react"
+import Display from "./components/Display"
+import Button from "./components/Button"
 import './App.css';
 
 function App() {
+  const [time, setTime]= useState({ms:0,s:0,m:0,h:0});
+  const [interv, setInterv]= useState();
+  const [status,setStatus]= useState(0)
+  const start=()=>{
+    run();
+    setStatus(1)
+    setInterv(setInterval(run,10));
+  };
+  var  updateMs=time.ms, updateM=time.m,updateS=time.s, updateH=time.h;
+  const run=()=>{
+    if(updateM===60){
+      updateH++;
+      updateM=0;
+    }
+    if(updateS===60){
+      updateM++;
+      updateS=0;
+    }
+    if(updateMs===100){
+      updateS++;
+      updateMs=0
+    }
+    updateMs++;
+    return setTime({ms:updateMs,s:updateS,m:updateM,h:updateH})
+  }
+  const stop=()=>{
+    clearInterval(interv);
+    setStatus(2)
+  };
+  const reset=()=>{
+    clearInterval(interv);
+    setStatus(0);
+    setTime({ms:0,s:0,m:0,h:0})
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="clock-holder">
+        <div className="stopwatch">
+          <Display time={time}/>         
+          <Button start={start} status={status} stop={stop} reset={reset}/>
+
+        </div>
+      </div>
+      
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Button(props) {
+    return (
+        <div>
+            {(props.status===0)? <button onClick={props.start} className="stopwatch-btn stopwatch-btn-gre" >Start</button>:""}
+            {(props.status===1)?
+            <div><button onClick={props.stop} className="stopwatch-btn stopwatch-btn-red" >Stop</button>
+            <button onClick={props.reset} className="stopwatch-btn stopwatch-btn-yel" >Reset</button></div>:""}
+            {(props.status===2)?
+            <div><button onClick={props.start} className="stopwatch-btn stopwatch-btn-gre" >Resume</button>
+            <button onClick={props.reset} className="stopwatch-btn stopwatch-btn-yel" >Reset</button></div>:""}
+            
+            
+            
+        </div>
+    )
+}
+export default Button;
